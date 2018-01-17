760c77bcf231efb6d7e5f1b7ef86ae03f80c903a
Robert Rossol <shyguyrbr1@gmail.com>
2018-01-17 17:46:49 -0600

    refactored problems 1-4

--- recursion.rb
+++ recursion.rb
@@ -4,39 +4,35 @@
 # 4. Write a recursive function that accepts an array of numbers and returns the sum.
 # 5. Write a recursive function that accepts a number and returns its factorial. (The factorial of 5, for example, is 5 * 4 * 3 * 2 * 1 = 120.)
 
-#1
+# 1
 def inception(x)
-  puts "INCEPTION"
-  if x > 0
-    inception(x-1)
-  end
+  puts 'INCEPTION'
+  inception(x - 1) if x > 0
 end
 inception(15)
 
-#2
+# 2
 def even(n)
-  n -= 1 if n % 2 != 0
-  if n >= 0
-    puts n - even(n - 2).to_i
-  end
+  n -= 1 if n.odd?
+  puts n - even(n - 2).to_i if n >= 0
 end
 
 even(100)
 
-#3
+# 3
 def sum(num)
-  total = num + sum(num-1).to_i if num > 0
+  num + sum(num - 1).to_i if num > 0
 end
 puts sum(10)
 
-#4
+# 4
 def array_sum(array)
-  if array.length ==1
-    array.shift 
+  if array.length == 1
+    array.shift
   else
     array.shift + array_sum(array)
   end
 end
-puts array_sum([5,6,8,10,23])
+puts array_sum([5, 6, 8, 10, 23])
 
-#5
+# 5
